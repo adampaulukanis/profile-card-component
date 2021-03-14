e6b058b66b1e0cb3733030e04a070c4cc0428ad1
Adam Paulukanis <adam.paulukanis@gmail.com>
2021-03-14 18:21:06 +0100

    Center the card

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
 "use strict";
 
-const suwak = document.querySelector('input[type="range"]');
 const log = document.getElementById("values");
 const card = document.querySelector("div.card");
-
-suwak.value = 0;
-
-const updateValue = (e) => {
-  log.textContent = e.target.value;
-  card.style.transform = `rotate(${e.target.value}deg)`;
-};
-
-suwak.addEventListener("input", updateValue);
